8d4c403260333ff4e2547ab71b06337fe7d32dc4
gergokutu <gergo.kutu@gmail.com>
2019-11-05 17:01:15 +0100

    SetTimeout applied to show the punchline delayed

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -4,11 +4,29 @@ import { incject, observer, inject } from 'mobx-react'
 @inject('JokeStore')
 @observer
 class Jokes extends Component {
+  state = {
+    punchline: false
+  }
+
   render() {
+    // how to solve that display an element after delay
+    // setTimeout works only rendered element
+    setTimeout(() => {
+      this.setState({punchline: true})
+    }, 2000) 
+
     return (
       <div className='joke'>
-        <p>{this.props.JokeStore.jokes.setup}</p>
-        <p>{this.props.JokeStore.jokes.punchline}</p>
+        {!this.state.punchline && 
+          <div>
+            <p>{this.props.JokeStore.jokes.setup}</p>
+          </div>}
+          
+        {this.state.punchline && 
+          <div>
+            <p>{this.props.JokeStore.jokes.setup}</p>
+            <p>{this.props.JokeStore.jokes.punchline}</p>
+          </div>}
       </div>
     )
   }
