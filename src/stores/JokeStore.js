ecdc75512dc9e46b7c7da50094efebe6f8a52bcb
gergokutu <gergo.kutu@gmail.com>
2019-12-27 10:34:04 +0100

    Random joke requested from API with superagent

--- src/stores/JokeStore.js
+++ src/stores/JokeStore.js
@@ -1,13 +1,14 @@
 import { observable, action, computed } from "mobx"
+import superagent from 'superagent'
 
 class JokeStore {
-  @observable jokes =
-    {
-      id: 1,
-      type: "programming",
-      setup: "What's the best thing about a Boolean?",
-      punchline: "Even if you're wrong, you're only off by a bit."
-    }
+  @observable jokes = {}
+    // {
+    //   id: 1,
+    //   type: "programming",
+    //   setup: "What's the best thing about a Boolean?",
+    //   punchline: "Even if you're wrong, you're only off by a bit."
+    // }
 
   @action addJoke = (joke) => {
     this.jokes.push(joke)
@@ -17,6 +18,19 @@ class JokeStore {
     return this.jokes.length
   }
 }
+
+
+// how to send the random jokes into @observable jokes ?
+// console.log('JokeStore:', JokeStore.jokes)
+superagent
+  .get('https://official-joke-api.appspot.com/jokes/programming/random')
+  .then(response => {
+    console.log(response.body)
+    this.JokeStore.jokes.push(response.body)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
 const store = new JokeStore()
 export default store
